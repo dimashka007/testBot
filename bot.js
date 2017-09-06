42c9e887fb9bd0ba33d6024b21403f92c08868e3
snork <0875695@gmail.com>
2017-09-06 13:30:47 +0000

    Now with working bot

---
+++ bot.js
@@ -0,0 +1,8 @@
+'use strict'
+const Telegram = require('node-telegram-bot-api')
+const parser = require('./parser')
+const tg = new Telegram(process.env.TOKEN)
+parser((url) => {
+   console.log(`Sending ${url} to @${process.env.CHANNEL}`)
+   tg.sendPhoto('@' + process.env.CHANNEL, url)
+})
